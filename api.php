6dcad7eba8df21363a2673b92c9613af9a7e4acc
mrawding <mrawding811@gmail.com>
2019-09-23 10:19:37 -0400

    Update api.php

--- api.php
+++ api.php
@@ -3,25 +3,11 @@
     //this is the basic way of getting a database handler from PDO, PHP's built in quasi-ORM
     $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
     if (!$dbhandle) die ($error);
- 
-    //this is a sample query which gets some data, the order by part shuffles the results
-    //the limit 0, 10 takes the first 10 results.
-    // you might want to consider taking more results, implementing "pagination", 
-    // ordering by rank, etc.
     $query = "SELECT rack, words FROM racks WHERE length=8 and weight <= 10 order by random() limit 0, 1";
-    
-    //this next line could actually be used to provide user_given input to the query to 
-    //avoid SQL injection attacks
     $statement = $dbhandle->prepare($query);
     $statement->execute();
-    
-    //The results of the query are typically many rows of data
-    //there are several ways of getting the data out, iterating row by row,
-    //I chose to get associative arrays inside of a big array
-    //this will naturally create a pleasant array of JSON data when I echo in a couple lines
     $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-    $rack = $results[0]["rack"]
-   // echo $stringRack;
+    $rack = $results[0]["rack"];
     echo $rack;
     //$query1 = "SELECT words FROM racks WHERE rack=.$rack";
     //$statement1 = $dbhandle->prepare($query1);
