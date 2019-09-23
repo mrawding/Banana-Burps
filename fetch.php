166c873416e5e5c0cb5ccdab85108005d52109fd
mrawding <mrawding811@gmail.com>
2019-09-23 00:29:01 -0400

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,8 +1,10 @@
 <?php
     $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
     if (!$dbhandle) die ($error);
-    $queries = $_SERVER['QUERY_STRING'];
-    echo($queries);
+    $json_queries = $_SERVER['QUERY_STRING'];
+    $php_obj = json_decode(json_queries);
+    printr($php_obj);
+    echo($php_obj[0]->text);
     $query = "SELECT rack, words FROM racks WHERE words LIKE '%$in%'";
     $statement = $dbhandle->prepare($query);
     $statement->execute();
