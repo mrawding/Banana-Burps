09215b5001bdf62c83fe8d062d7e3cc6567b3b1e
mrawding <mrawding811@gmail.com>
2019-09-23 01:57:34 -0400

    Update api.php

--- api.php
+++ api.php
@@ -22,7 +22,7 @@
     $results = $statement->fetchAll(PDO::FETCH_ASSOC);
     $rack = $results[0]["rack"];
     echo $rack;
-    $query1 = "SELECT * FROM words WHERE length >= 3 and rack LIKE %"$rack"%";
+    $query1 = "SELECT * FROM words WHERE length >= 3 and rack=%"$rack"%";
     $statement1 = $dbhandle->prepare($query1);
     $statement1->execute();
     $results1 = $statement1->fetchAll(PDO::FETCH_ASSOC);
