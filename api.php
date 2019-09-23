333a2f1681babee7098768c73b8dc20180c8665e
mrawding <mrawding811@gmail.com>
2019-09-23 01:17:24 -0400

    Update api.php

--- api.php
+++ api.php
@@ -20,7 +20,7 @@
     //I chose to get associative arrays inside of a big array
     //this will naturally create a pleasant array of JSON data when I echo in a couple lines
     $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-    $rack = $results[0{"rack"}];
+    $rack = $results[0]['rack'];
     echo $rack;
     echo json_encode($results);
     $query1 = "SELECT * FROM words WHERE length >= 3 and rack LIKE %'$rack'%";
