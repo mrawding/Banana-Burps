4ab0d28b7a7515deefbdf08b5b51ac81a8e73c6b
mrawding <mrawding811@gmail.com>
2019-09-27 16:58:57 -0400

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -3,10 +3,10 @@ session_start();
 $current_score = [];
 $curent_score[] = explode("x=",$_SERVER['QUERY_STRING']);
 if (!isset($_SESSION["highscore"])){
-    $_SESSION["highscore"] = "0";
+    $_SESSION["highscore"] = 0;
 } else {
-    if($current_score[0][1] > $_SESSION["highscore"]){
-            $_SESSION["highscore"] = $currrent_score[0][1];
+    if((int)$current_score[0][1] > $_SESSION["highscore"]){
+            $_SESSION["highscore"] = (int)$currrent_score[0][1];
          }
 }
     
