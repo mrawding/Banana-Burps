16191900122c4ca73aff2c2c1fe14e1bbaf89895
mrawding <mrawding811@gmail.com>
2019-09-27 09:43:14 -0400

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,9 +1,14 @@
 <?php
     session_start();
     $curent_score = json_decode($_GET["x"], false);
+    if (!isset($_SESSION["HighScore"])){
     $_SESSION["HighScore"] = 0;
-    if($current_score > $_SESSION["HighScore"])
-    $_SESSION["HighScore"] = $currrent_score;
+    }
+    else{
+    if($current_score > $_SESSION["HighScore"]){
+        $_SESSION["HighScore"] = $currrent_score;
+    }
+    }
     echo json_encode($_SESSION["HighScore"]);
     header('HTTP/1.1 200 OK');
     header('Content-Type: application/json');
