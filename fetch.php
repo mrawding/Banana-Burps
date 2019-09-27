c11811f5fa766d4a261eeb2dce7213cbeb17562e
mrawding <mrawding811@gmail.com>
2019-09-27 16:48:20 -0400

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,17 +1,16 @@
 <?php
-    header("Content-Type: application/json; charset=UTF-8");
-    session_start();
-    $current_score = [];
-    $curent_score[] = explode("x=",$_SERVER['QUERY_STRING']);
-    
-    
-    if (!isset($_SESSION["HighScore"])){
+session_start();
+$current_score = [];
+$curent_score[] = explode("x=",$_SERVER['QUERY_STRING']);
+if (!isset($_SESSION["highscore"])){
     $_SESSION["HighScore"] = "0";
-    }else{
-        if($current_score[0][1] > $_SESSION["HighScore"]){
-            $_SESSION["HighScore"] = $currrent_score[0][1];
+} else {
+    if($current_score[0][1] > $_SESSION["highscore"]){
+            $_SESSION["highscore"] = $currrent_score[0][1];
          }
-    }
+}
+    
+    
     echo json_encode($_SESSION["HighScore"]);
     header('HTTP/1.1 200 OK');
     header('Content-Type: application/json');
