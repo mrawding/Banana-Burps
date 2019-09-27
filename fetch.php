a35d01ececb7a1a3b95d9ca9b7d01d70de1efb1f
mrawding <mrawding811@gmail.com>
2019-09-27 09:38:42 -0400

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -3,8 +3,8 @@
     $curent_score = json_decode($_GET["x"], false);
     $_SESSION("HighScore") = 0;
     if($current_score > $_SESSION("HighScore")
-       $_SESSION("HighScore") = $currrent_score;
-    echo(json_encode($_SESSION("HighScore");
+    $_SESSION("HighScore") = $currrent_score;
+    echo json_encode($_SESSION("HighScore"));
     header('HTTP/1.1 200 OK');
     header('Content-Type: application/json');
    // echo json_encode($results);
