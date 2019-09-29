dcdab0530e9e3d73d300ffe71f40a924d4f5163a
mrawding <mrawding811@gmail.com>
2019-09-29 19:41:14 -0400

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -5,6 +5,8 @@ $cookie_value = 0;
 $curent_score[] = explode("x=",$_SERVER['QUERY_STRING']);
 setcookie($cookie_name, $cookie_value);
 echo $current_score;
+?>
+<?php
 if(!isset($_COOKIE[$cookie_name])) {
     echo json_encode $cookie_value;
 } else {
