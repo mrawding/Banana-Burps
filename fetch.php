069a72932abffb5064663bc7cbf48ecf666d27b2
mrawding <mrawding811@gmail.com>
2019-09-29 19:25:08 -0400

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,17 +1,19 @@
 <?php
-session_start();
-$current_score = [];
+
+$cookie_name = "Highscore";
+$cookie_value = 0;
 $curent_score[] = explode("x=",$_SERVER['QUERY_STRING']);
-if (!isset($_SESSION["highscore"])){
-    $_SESSION["highscore"] = 0;
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+$score = (int) current_score[0][1];
+if(!isset($_COOKIE[$cookie_name])) {
+    echo json_encode[$cookie_value];
 } else {
-    if((int)$current_score[0][1] > $_SESSION["highscore"]){
-            $_SESSION["highscore"] = (int)$currrent_score[0][1];
-         }
+    if($_COOKIE[$cookie_value] < $score){
+        $cookie_value = $score;
+        setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
+        echo json_encode[$_COOKIE[$cookie_value];
 }
     
-    
-    echo json_encode($_SESSION["highscore"]);
     header('HTTP/1.1 200 OK');
     header('Content-Type: application/json');
    // echo json_encode($results);
