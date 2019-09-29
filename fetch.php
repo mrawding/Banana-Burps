0f66f267d22deb9458bfa406efa91f4f689c1350
mrawding <mrawding811@gmail.com>
2019-09-29 19:32:57 -0400

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -4,7 +4,7 @@ $cookie_name = "Highscore";
 $cookie_value = 0;
 $curent_score[] = explode("x=",$_SERVER['QUERY_STRING']);
 setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
-$score = (int) current_score[0][1];
+$score = ((integer)current_score[0][1]);
 if(!isset($_COOKIE[$cookie_name])) {
     echo json_encode $cookie_value;
 } else {
