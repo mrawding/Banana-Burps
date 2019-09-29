19b1dedd32b8b2a6da49104fef1b9630053b682f
mrawding <mrawding811@gmail.com>
2019-09-29 19:28:37 -0400

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -6,12 +6,12 @@ $curent_score[] = explode("x=",$_SERVER['QUERY_STRING']);
 setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
 $score = (int) current_score[0][1];
 if(!isset($_COOKIE[$cookie_name])) {
-    echo json_encode[$cookie_value];
+    echo json_encode $cookie_value;
 } else {
     if($_COOKIE[$cookie_value] < $score){
         $cookie_value = $score;
         setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
-        echo json_encode[$_COOKIE[$cookie_value];
+        echo json_encode $_COOKIE[$cookie_value];
 }
     
     header('HTTP/1.1 200 OK');
