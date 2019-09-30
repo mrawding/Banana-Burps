74e0d9f37466d0c1f85550986fa1424f5261570a
mrawding <mrawding811@gmail.com>
2019-09-29 21:53:57 -0400

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -11,15 +11,16 @@ $score_int = $score->$score_key;
 
 //echo json_encode $current_score[0][1];
 if(!isset($_COOKIE[$cookie_name])) {
-    echo $cookie_value;
+    echo json_encode($cookie_value);
 } else {
     if($_COOKIE[$cookie_value] <= $score_int){
         $cookie_value = $score_int;
          setcookie($cookie_name, $cookie_value);
          
     }
+    echo json_encode($_COOKIE[$cookie_value]);
 }
-echo $_COOKIE[$cookie_value];
+
     
     header('HTTP/1.1 200 OK');
   
