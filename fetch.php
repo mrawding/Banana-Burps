6dc5f4c2988b6959fb2939d856760afd6440e359
mrawding <mrawding811@gmail.com>
2019-09-29 20:34:02 -0400

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -5,7 +5,7 @@ $cookie_value = 0;
 $score = $_GET["score"];
 $scoew = json_decode($score);
 setcookie($cookie_name, $cookie_value);
-echo  $score ;
+echo "what the fuck .$score " ;
 //echo json_encode $current_score[0][1];
 //if(!isset($_COOKIE[$cookie_name])) {
   //  echo json_encode $cookie_value;
